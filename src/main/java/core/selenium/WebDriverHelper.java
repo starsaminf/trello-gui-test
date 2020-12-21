45dfb6de379ebf55af44bf55ad3a0d5f9b201112
Samuel Loza <starsaminf@gmail.com>
2020-12-21 06:35:22 -0400

    Add core selenium files

---
+++ src/main/java/core/selenium/WebDriverHelper.java
@@ -0,0 +1,54 @@
+package core.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * This class is helper of WebDriverManager class
+ */
+public final class WebDriverHelper {
+
+    private WebDriverHelper() {
+
+    }
+
+    /**
+     * Sets a text in the webElement
+     * @param webElement
+     * @param text
+     */
+    public static void setElement(final WebElement webElement, final String text) {
+        webElement.clear();
+        webElement.sendKeys(text);
+    }
+
+    /**
+     * Sets a text in the By.
+     * @param by
+     * @param text
+     */
+    public static void setElement(final By by, final String text) {
+        WebDriver webDriver = WebDriverManager.getInstance().getWebDrive();
+        WebElement webElement = webDriver.findElement(by);
+        setElement(webElement, text);
+    }
+
+    /**
+     * Clicks an element
+     * @param webElement
+     */
+    public static void clickElement(final WebElement webElement) {
+        WebDriverWait webDriverWait = WebDriverManager.getInstance().getWebDriverWait();
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(webElement));
+        webElement.click();
+    }
+
+    public static void clickElement(final By by) {
+        WebDriver webDriver =WebDriverManager.getInstance().getWebDrive();
+        WebElement webElement = webDriver.findElement(by);
+        clickElement(webElement);
+    }
+}
---
+++ src/main/java/core/selenium/config/Environment.java
@@ -0,0 +1,37 @@
+package core.selenium.config;
+
+/**
+ * PropertiesReader class.
+ */
+public final class Environment extends EnvironmentBase{
+
+    private static final String PATH = "gradle.properties";
+    private static Environment environment;
+
+    /**
+     * Initializes an instance of Environment Chrome
+     */
+    private Environment() {
+        super(PATH);
+    }
+
+    /**
+     * get instance or create a new one.
+     *
+     * @return PropertiesReader instance.
+     */
+    public static Environment getInstance() {
+        if (environment == null) {
+            environment = new Environment();
+        }
+        return environment;
+    }
+
+    /**
+     * get the cucumberThreadCount from the file.properties.
+     * @return cucumberThreadCount value.
+     */
+    public String getCucumberThreadCount() {
+        return getEnvProperty("cucumberThreadCount");
+    }
+}
