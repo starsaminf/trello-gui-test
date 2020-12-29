cd9aca6598c73ca0494c2a04a7298d3f388d57ca
Samuel Loza <starsaminf@gmail.com>
2020-12-29 12:03:43 -0400

    Add chrome install script

---
+++ scripts/chrome_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --no-verbose -O /tmp/chrome.deb wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_83.0.4103.116-1_amd64.deb \
+  && apt install -y /tmp/chrome.deb \
+  && rm /tmp/chrome.deb
