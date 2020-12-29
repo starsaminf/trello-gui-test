ff5bf8adf3c4cf508079bc65996236030f30c1d9
Samuel Loza <starsaminf@gmail.com>
2020-12-29 13:30:00 -0400

    Add wget command

--- scripts/chrome_install.sh
+++ scripts/chrome_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt update && sudo apt install wget
 wget --no-verbose -O /tmp/chrome.deb wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_83.0.4103.116-1_amd64.deb \
   && apt install -y /tmp/chrome.deb \
   && rm /tmp/chrome.deb
