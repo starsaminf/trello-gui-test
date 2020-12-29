3a9ab93d5c316f99efcfd7649a2d08683a73d51f
Samuel Loza <starsaminf@gmail.com>
2020-12-29 13:43:27 -0400

    Update chrome version to 87.0.4280

--- scripts/chrome_install.sh
+++ scripts/chrome_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_83.0.4103.116-1_amd64.deb \
+wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_87.0.4280.88-1_amd64.deb \
   && sudo dpkg -i /tmp/chrome.deb \
   && sudo apt install -f \
   && rm /tmp/chrome.deb
