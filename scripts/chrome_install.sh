b17d10848257971d0893e7ddb92739335ed7b533
Samuel Loza <starsaminf@gmail.com>
2020-12-29 13:34:24 -0400

    Change CLI interface by apt-get

--- scripts/chrome_install.sh
+++ scripts/chrome_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_83.0.4103.116-1_amd64.deb \
-  && apt install -y /tmp/chrome.deb \
+  && sudo apt-get install -y /tmp/chrome.deb \
   && rm /tmp/chrome.deb
